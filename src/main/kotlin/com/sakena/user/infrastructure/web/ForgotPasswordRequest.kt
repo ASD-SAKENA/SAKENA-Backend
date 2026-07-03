8784a6342538180d01cd566243aaa0f5843cb3e1
melikaalizade <melikaalizadeh7@gmail.com>
2026-07-03 13:31:09 +0330

    feat(web): add ForgotPasswordRequest DTO

---
+++ src/main/kotlin/com/sakena/user/infrastructure/web/ForgotPasswordRequest.kt
@@ -0,0 +1,10 @@
+package com.sakena.user.infrastructure.web
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
+data class ForgotPasswordRequest(
+    @field:NotBlank
+    @field:Email
+    val email: String
+)
