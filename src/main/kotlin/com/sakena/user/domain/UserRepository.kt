8538d5000777792917cb48c37d0870a03a5e4ba6
melikaalizade <melikaalizadeh7@gmail.com>
2026-07-03 13:29:56 +0330

    feat(domain): add findById method to UserRepository interface

--- src/main/kotlin/com/sakena/user/domain/UserRepository.kt
+++ src/main/kotlin/com/sakena/user/domain/UserRepository.kt
@@ -4,6 +4,7 @@ interface UserRepository {
     fun save(user: User): User
     fun findByUsername(username: String): User?
     fun findByEmail(email: String): User?
+    fun findById(id: UserId): User?
     fun existsByUsername(username: String): Boolean
     fun existsByEmail(email: String): Boolean
 }
