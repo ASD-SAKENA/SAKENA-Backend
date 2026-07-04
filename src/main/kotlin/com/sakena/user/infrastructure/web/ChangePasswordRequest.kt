c2b71e34a9d4e1db12b8fd2d44b33755378f73dc
melikaalizade <melikaalizadeh7@gmail.com>
2026-07-04 16:38:32 +0330

    feat(web): add ChangePasswordRequest DTO for password change endpoint

---
+++ src/main/kotlin/com/sakena/user/infrastructure/web/ChangePasswordRequest.kt
@@ -0,0 +1,13 @@
+package com.sakena.user.infrastructure.web
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class ChangePasswordRequest(
+    @field:NotBlank
+    val currentPassword: String,
+
+    @field:NotBlank
+    @field:Size(min = 8)
+    val newPassword: String
+)
