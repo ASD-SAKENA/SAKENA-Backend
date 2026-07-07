bfcd2830eeebd8552b57e852874f96d3f99f26bc
Amir Hossein Shahidi <amirahsh20041382@gmail.com>
2026-07-07 13:39:44 +0330

    added get all services for admin(MANAGER)

--- src/main/kotlin/com/sakena/servicerequest/infrastructure/persistence/JpaServiceRequestRepository.kt
+++ src/main/kotlin/com/sakena/servicerequest/infrastructure/persistence/JpaServiceRequestRepository.kt
@@ -1,8 +1,9 @@
 package com.sakena.servicerequest.infrastructure.persistence
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import java.util.UUID
 
-interface JpaServiceRequestRepository : JpaRepository<ServiceRequestJpaEntity, UUID> {
+interface JpaServiceRequestRepository : JpaRepository<ServiceRequestJpaEntity, UUID>, JpaSpecificationExecutor<ServiceRequestJpaEntity> {
     fun findAllByCreatedBy(createdBy: UUID): List<ServiceRequestJpaEntity>
 }
